9e9596f38e7f5d7dbfcd8f521daa0bcb3d1420fd
Indi <indi.lefay@gmail.com>
2025-04-24 16:01:52 -0300

    Create Loops and Lists.py

---
+++ Loops and Lists.py
@@ -0,0 +1,4 @@
+from learntools.core import binder; binder.bind(globals()) # bibliotecas usadas no Kaggle para verificar as respostas dos usuarios
+from learntools.python.ex5 import *
+print('Setup complete.')
+
